4a0688c867ea5ab0313209380698fd2e4b942e70
KarlinCoder <giancarlo.frontend@gmail.com>
2024-12-10 04:48:12 -0500

    adding navbar icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,12 +12,21 @@ const MENU_ITEMS: Array<MenuItem> = [
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="flex flex-col md:flex-row sticky z-50 top-0 justify-evenly items-center border-b-[1px] border-yellow-800 bg-[#111111d8] backdrop-blur-xl px-8">
-      <section className="flex flex-col text-neutral-500 mt-1 px-5 py-4 md:py-6 md:p-0 text-center md:text-start ">
-        <h2 className="text-[1.50rem] font-carltine-cm text-yellow-600 leading-4">
-          KarlinCoder
-        </h2>
-        <p className="text-sm leading-4">Web Frontend Developer</p>
+    <nav className="flex flex-col md:flex-row sticky z-50 top-0 justify-evenly items-center border-b-[1px] border-yellow-800 bg-[#111111d8] backdrop-blur-xl px-8 py-3">
+      <section className="flex items-center justify-center gap-2">
+        <div className="w-[43px] h-[43px] overflow-hidden rounded-full">
+          <img
+            src="/images/avatar_icon.webp"
+            alt="website icon"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col text-neutral-500 mt-1 text-center md:text-start ">
+          <h2 className="text-[1.50rem] font-carltine-cm text-yellow-600 leading-4">
+            KarlinCoder
+          </h2>
+          <p className="text-sm leading-4">Web Frontend Developer</p>
+        </div>
       </section>
 
       <aside className="hidden md:block">
@@ -27,7 +36,7 @@ export const Navbar: React.FC = () => {
               <li key={item.text}>
                 <a
                   href={item.link}
-                  className="inline-block px-5 py-6 text-neutral-300 hover:text-[#df9d47] active:text-neutral-300 hover:underline hover:underline-offset-8 "
+                  className="inline-block px-3 text-neutral-300 hover:text-[#df9d47] active:text-neutral-300 hover:underline hover:underline-offset-8 "
                 >
                   {item.text}
                 </a>
