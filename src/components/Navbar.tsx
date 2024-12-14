98a0bae37f4fa32224c47d3006896b55c8ff534e
KarlinCoder <giancarlo.frontend@gmail.com>
2024-12-14 01:20:05 -0500

    adding sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Sidebar from "./sections/Sidebar";
+import { HiMenu, HiMenuAlt1 } from "react-icons/hi";
+
 interface MenuItem {
   text: string;
   link: string;
@@ -11,8 +15,18 @@ const MENU_ITEMS: Array<MenuItem> = [
 ];
 
 export const Navbar: React.FC = () => {
+  const [showMobileSidebar, setShowMobileSidebar] = useState(false);
+
+  const handleShowModal = () => {
+    setShowMobileSidebar((value) => !value);
+  };
+
+  const handleCloseModal = (value: boolean) => {
+    setShowMobileSidebar(value);
+  };
+
   return (
-    <nav className="flex flex-col md:flex-row sticky z-50 top-0 justify-evenly items-center border-b-[1px] border-yellow-800 bg-[#111111d8] backdrop-blur-xl px-8 py-3">
+    <nav className="flex sticky z-50 top-0  justify-between md:justify-evenly items-center border-b-[1px] border-yellow-800 bg-[#111111d8] backdrop-blur-xl px-8 py-3">
       <section className="flex items-center justify-center gap-2">
         <div className="w-[43px] h-[43px] overflow-hidden rounded-full">
           <img
@@ -28,6 +42,18 @@ export const Navbar: React.FC = () => {
           <p className="text-sm leading-4">Web Frontend Developer</p>
         </div>
       </section>
+
+      {showMobileSidebar && <Sidebar handleShow={handleCloseModal} />}
+
+      {!showMobileSidebar ? (
+        <button className="block md:hidden" onClick={handleShowModal}>
+          <HiMenu className="text-[1.9rem] text-neutral-100" />
+        </button>
+      ) : (
+        <button className="block md:hidden" onClick={handleShowModal}>
+          <HiMenuAlt1 className="text-[1.9rem] text-neutral-100" />
+        </button>
+      )}
 
       <aside className="hidden md:block">
         <ul className="flex flex-wrap text-neutral-200 text-[0.97rem]">
---
+++ src/components/sections/Sidebar.tsx
@@ -0,0 +1,69 @@
+import ReactDOM from "react-dom";
+import { FaCubesStacked } from "react-icons/fa6";
+import { PiUserList } from "react-icons/pi";
+import { RiBookShelfLine, RiUser2Fill } from "react-icons/ri";
+
+interface SidebarProps {
+  handleShow: (value: boolean) => void;
+}
+
+const linkClasses =
+  "flex justify-start gap-3 items-center text-[1rem] px-5 py-3 cursor-pointer hover:bg-blue-900 hover:bg-opacity-80  active:bg-blue-800 active:bg-opacity-20 rounded-md";
+const iconClasses = "text-[2rem]";
+
+const Sidebar = ({ handleShow }: SidebarProps) => {
+  return ReactDOM.createPortal(
+    <aside
+      onClick={() => handleShow(false)}
+      className={`fixed flex justify-end items-center z-[99] top-0 sidebar-cm text-white w-full sidebar-cm bg-gradient-to-l from-[#00000077] from-98% to-[#00000010]`}
+    >
+      <nav className="flex p-5 flex-col  justify-center w-full max-w-[270px] h-dvh bg-blue-950 shadow-[0_0_40px_#0008]">
+        <ul>
+          <li>
+            <a
+              className={linkClasses}
+              onClick={() => handleShow(false)}
+              href="#experiencia"
+            >
+              <PiUserList className={iconClasses} />
+              <p>Experiencia</p>
+            </a>
+          </li>
+          <li>
+            <a
+              className={linkClasses}
+              onClick={() => handleShow(false)}
+              href="#skills"
+            >
+              <FaCubesStacked className={iconClasses} />
+              <p>Skills</p>
+            </a>
+          </li>
+          <li>
+            <a
+              className={linkClasses}
+              onClick={() => handleShow(false)}
+              href="#proyectos"
+            >
+              <RiBookShelfLine className={iconClasses} />
+              <p>Proyectos</p>
+            </a>
+          </li>
+          <li>
+            <a
+              className={linkClasses}
+              onClick={() => handleShow(false)}
+              href="#personal"
+            >
+              <RiUser2Fill className={iconClasses} />
+              <p>RiUser2Fill</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>,
+    document.body
+  );
+};
+
+export default Sidebar;
